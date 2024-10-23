8fad2119bff7cfb8bd0ef04194bc207441638d25
arose114 <akinolakemi114@gmail.com>
2024-10-23 18:00:41 +0100

    navbar with tailwind

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="w-full mx-auto max-w-7xl bg-white">
+      <header className="">
+      <nav>
+        <div className="hidden md:flex justify-between items-center">
+        <a href="#" className="font-bold" >Logo</a>
+        <ul className="flex">
+          <li className="p-4"><a href="3">Home</a></li>
+          <li  className="p-4"><a href="3">About</a></li>
+          <li  className="p-4"><a href="3">Services</a></li>
+          <li  className="p-4"><a href="3">Contact</a></li>
+
+        </ul>
+        </div>
+
+        <div className="fixed bg-red-200 justify-between items-center md:hidden">
+        <a href="#" className="font-bold" >Logo</a>
+        <ul className="pt-5 left-0 top-0">
+          <li className="p-4"><a href="3">Home</a></li>
+          <li  className="p-4"><a href="3">About</a></li>
+          <li  className="p-4"><a href="3">Services</a></li>
+          <li  className="p-4"><a href="3">Contact</a></li>
+
+        </ul>
+        </div>
+      </nav>
+     
       </header>
     </div>
   );
